7d4affcfa47b18f91b73f405cd71a3ae672e016f
BuildTools <unconfigured@null.spigotmc.org>
2022-08-14 17:28:14 +0900

    commit by skyexcel

--- src/main/java/me/github/freejia/Main.java
+++ src/main/java/me/github/freejia/Main.java
@@ -1,4 +1,10 @@
 package me.github.freejia;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        super.onEnable();
+    }
 }
