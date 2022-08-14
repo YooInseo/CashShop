decaa1b087cfd874e8c3dadba7e1df20c14993a0
idkNicks <yangdaeyeong0808@gmail.com>
2022-08-14 17:32:22 +0900

    commit by idkNick

--- src/main/java/me/github/freejia/Main.java
+++ src/main/java/me/github/freejia/Main.java
@@ -1,10 +1,18 @@
 package me.github.freejia;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Logger;
 
 public class Main extends JavaPlugin {
 
+    private static final Logger Log = Bukkit.getLogger();
+
     public void onEnable() {
         super.onEnable();
+
+        Log.info("플러그인이 켜졌다옹");
+
     }
 }
