83a9fe187d28c92ac86a37ed4054f43256825c04
BuildTools <unconfigured@null.spigotmc.org>
2022-08-15 18:06:55 +0900

    commit by skyexcel

--- src/main/java/me/github/freejia/cmd/tab/CashTabComplete.java
+++ src/main/java/me/github/freejia/cmd/tab/CashTabComplete.java
@@ -1,5 +1,6 @@
 package me.github.freejia.cmd.tab;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
@@ -8,6 +9,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class CashTabComplete implements TabCompleter {
@@ -19,17 +21,33 @@ public class CashTabComplete implements TabCompleter {
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
         Player player = (Player)sender;
+
         if(args.length == 1){
             if(player.isOp()){
-                results.add("지급");
-                results.add("제거");
-                results.add("설정");
-                results.add("확인");
+                if(results.isEmpty()){
+                    results.add("지급");
+                    results.add("제거");
+                    results.add("설정");
+                    results.add("확인");
+                }
+            }
+            return results;
+        } else if (args.length == 2 && args.length != 1){
+            results.remove("지급");
+            results.remove("제거");
+            results.remove("설정");
+            results.remove("확인");
+
+            for(Player online : Bukkit.getOnlinePlayers()){
+                results.add(online.getDisplayName());
             }
 
-
-
             return results;
+        } else if(args.length == 3 && args.length != 2 ){
+            if(!results.isEmpty()){
+                results.removeAll(results);
+            }
+            results.add("int");
         }
 
         return results;
